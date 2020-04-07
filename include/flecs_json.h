742c378227cd056bea3f998671ceed37529ac7ec
Sander Mertens <sander.mertens8@gmail.com>
2020-04-07 02:52:50 -0700

    Initial commit

---
+++ include/flecs_json.h
@@ -0,0 +1,18 @@
+#ifndef FLECS_JSON_H
+#define FLECS_JSON_H
+
+/* This generated file contains includes for project dependencies */
+#include "flecs-json/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char* ecs_to_json(ecs_world_t *world, ecs_entity_t e);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
